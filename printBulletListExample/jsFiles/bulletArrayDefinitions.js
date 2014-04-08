a8c5260c44207dc0ad5a09cdda0168087c598beb
Waqar M <waqar.mahmood.pro@gmail.com>
2014-04-08 15:32:48 -0500

    First commit. Edited a bullet in benefits.

--- printBulletListExample/jsFiles/bulletArrayDefinitions.js
+++ printBulletListExample/jsFiles/bulletArrayDefinitions.js
@@ -39,7 +39,7 @@ nameArray = new Array(
 benefitsArray = new Array (
     "All user has to do to create a bullet list is to create a text file that provides content of each bullet. See bulletArrayDefinitions.js.",
     "The function automatically determines how many bullets there are and generates the html code.",
-    "Adding bullets is as easy as adding its value into the text file in sequence.",
+    "Adding a bullet is as easy as adding its value into the text file in sequence in which it is to appear.",
     "It allows an easy way to print bullet list in columns--user specifies how many columns.",
     "For convenience, user may add heading and heading level (h1, h2, etc) via the function, or add them separately."
 );
